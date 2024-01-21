5d0e22c6b5952dd9cfaf628a321e830f0656fbc9
W9pi3cZ1 <ghg13368@163.com>
2024-01-21 15:16:19 +0800

    Update | Better ui, and better file structure

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,18 @@ fn main() {
     let handle2 = main.as_weak();
     main.on_move_win(move |offset_x,offset_y| {
         let main = handle2.upgrade().unwrap();
+        main.window().set_position(LogicalPosition::new(offset_x,offset_y));
+    });
+
+    let handle3 = main.as_weak();
+    main.on_get_win_pos(move || {
+        let main = handle3.upgrade().unwrap();
         let logical_pos = main.window().position().to_logical(main.window().scale_factor());
-        main.window().set_position(LogicalPosition::new(logical_pos.x + offset_x,logical_pos.y + offset_y));
+        (logical_pos.x,logical_pos.y)
+    });
+
+    main.on_dbug(move |val| {
+        println!("{val}")
     });
 
     main.run().unwrap();
